9e6158ecf5a35a13dc8d763d67e54a23569fb3da
Luis <luisfernandosantaospina@gmail.com>
2022-11-06 16:19:45 -0500

    Finaliza monkey con eventos aleatorios

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+M
+cypress/fixtures/example.json,7\2\72a61609985a07cf3d4e451ab16e1dcb751ceec9
+F
+cypress/support/e2e.js,c\c\ccce1f3426de57a34f01ae026a2f13007c767429
--- cypress/e2e/monkey_testing.spec.cy.js
+++ cypress/e2e/monkey_testing.spec.cy.js
@@ -1,9 +1,4 @@
 describe('Los estudiantes under monkeys', function() {
-  it('visits los estudiantes and survives monkeys with clicks', function() {
-    cy.visit('https://losestudiantes.co');
-    cy.wait(1000);
-    randomClick(10);
-  })
   it.only('visits los estudiantes and survives monkeys with events', function() {
     cy.visit('https://losestudiantes.co');
     cy.wait(1000);
@@ -11,50 +6,66 @@ describe('Los estudiantes under monkeys', function() {
   })
 })
 
-function randomClick(monkeysLeft) {
+function randomEvent(numberOfEvents) {
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  if(monkeysLeft > 0) {
-    cy.get('a').then($links => {
-      let randomLink = $links.get(getRandomInt(0, $links.length));
-      if(!Cypress.dom.isHidden(randomLink)) {
-        cy.wrap(randomLink).click({force: true});
-        monkeysLeft = monkeysLeft - 1;
+  if(numberOfEvents > 0) {
+    cy.window().then((win)=>
+    {
+      let event = getRandomInt(1,4);
+      switch (event) {
+        case 1:
+          let a = win.document.getElementsByTagName("a");
+          if(a.length > 0){
+            cy.get('a').then($links => {
+              let randomLink = $links.get(getRandomInt(0, $links.length));
+              if(!Cypress.dom.isHidden(randomLink)) {
+                cy.wrap(randomLink).click({force: true});
+              }
+            });
+          }
+          break;
+        case 2:
+          let inputs = win.document.getElementsByTagName("input");
+          if(inputs.length > 0){
+            cy.get('input').then($fields => {
+              let randomField = $fields.get(getRandomInt(0, $fields.length));
+              if(!Cypress.dom.isHidden(randomField)) {
+                cy.wrap(randomField).type('monkey', { force: true });
+              }
+            });
+          }
+          break;
+        case 3:
+          let selects = win.document.getElementsByTagName("select");
+          if(selects.length > 0){
+            cy.get('select').then($fields => {
+              let randomField = $fields.get(getRandomInt(0, $fields.length));
+              if(!Cypress.dom.isHidden(randomField)) {
+                cy.wrap(randomField).select(getRandomInt(0,1),{force: true});
+              }
+            });
+          }
+          break;
+        case 4:
+          let buttons = win.document.getElementsByTagName("button");
+          if(buttons.length > 0){
+            cy.get('button').then($fields => {
+              let randomField = $fields.get(getRandomInt(0, $fields.length));
+              if(!Cypress.dom.isHidden(randomField)) {
+                cy.wrap(randomField).click({ force: true });
+              }
+            });
+          }
+          break;
       }
-      cy.wait(1000);
-      randomClick(monkeysLeft);
     });
-  }
-}
-
-function randomEvent(monkeysLeft) {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  function getRandomEvent() {
-    let events = [
-        'click',
-        'type'
-    ]
-    return 'cy.wrap(randomLink).' + events[0] + '({force: true});'
-  }
-
-  if(monkeysLeft > 0) {
-    cy.get('a').then($links => {
-      let randomLink = $links.get(getRandomInt(0, $links.length));
-      if(!Cypress.dom.isHidden(randomLink)) {
-        getRandomEvent()
-        monkeysLeft = monkeysLeft - 1;
-      }
-      cy.wait(1000);
-      randomClick(monkeysLeft);
-    });
+    numberOfEvents = numberOfEvents - 1;
+    cy.wait(500);
+    randomEvent(numberOfEvents);
   }
 }
